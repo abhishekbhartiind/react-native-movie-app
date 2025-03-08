85edde0feb560e02f4dd6487641041eef8090238
Abhishek Bharti <abhishek.bharti@3insys.com>
2025-03-08 20:46:55 +0530

    movie application

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'expo-router'
 import { View, Text } from 'react-native'
 
 const Home = () => {
   return (
     <View className='flex-1 items-center justify-center'>
       <Text className='text-5xl text-dark-200 font-bold'>Home</Text>
+      <Link href='/onboarding' className='text-accent font-bold'>Onboarding</Link>
     </View>
   )
 }

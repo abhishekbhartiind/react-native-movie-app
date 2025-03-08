f481778f2a8fac4213f05639078c25e47678e764
Abhishek Bharti <abhishek.bharti@3insys.com>
2025-03-08 20:52:36 +0530

    movie application

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from "react-native"
 import React from 'react'
+import { images } from '@/constants/images'
 
 const Index = () => {
   return (
-    <View>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
+
       <Text>Index</Text>
     </View>
   )

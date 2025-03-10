7f37fa1d7f193c0dc57bdeccfcfc567873966819
Abhishek Bharti <abhishek.bharti@3insys.com>
2025-03-10 21:57:29 +0530

    React Native Movie App

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,12 @@ import { StyleSheet, Text, View, Image, ScrollView } from "react-native"
 import React from 'react'
 import { images } from '@/constants/images'
 import { icons } from "@/constants/icons"
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router"
 
 const Index = () => {
+    const router = useRouter()
+
   return (
     <View className="flex-1 bg-primary">
       <Image
@@ -17,6 +21,9 @@ const Index = () => {
         contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
       >
           <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+          <View className='flex-1 px-5'>
+              <SearchBar placeholder={"Search for a movie"} onPress={() => router.push("/search")} />
+          </View>
       </ScrollView>
     </View>
   )

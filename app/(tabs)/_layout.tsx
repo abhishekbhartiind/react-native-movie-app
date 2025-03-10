94b78826e74464b62ed9e7e73013707a8b2da017
Abhishek Bharti <abhishek.bharti@3insys.com>
2025-03-10 13:38:36 +0530

    movie app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,27 @@
-import { Image, ImageBackground, Text } from 'react-native'
+import { Image, ImageBackground, Text, View } from 'react-native'
 import { Tabs } from 'expo-router'
 import { images } from '@/constants/images'
 import { icons } from '@/constants/icons'
 
 const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused)
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    )
+
   return (
-    <ImageBackground
-      source={images.highlight}
-      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-    >
-      <Image source={icon} className="size-5" tintColor={"#151312"} />
-      <Text className="text-secondary font-semibold ml-2 text-base">
-        {title}
-      </Text>
-    </ImageBackground>
-  )
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
 }
 
 const _Layout = () => {
---
+++ app/movie/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'expo-router'
+import { View, Text } from 'react-native'
+
+const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-dark-200 font-bold">Home</Text>
+      <Link href="/onboarding" className="text-accent font-bold">
+        Onboarding
+      </Link>
+      <Link href="/movie/avanger" className="text-accent font-bold">
+        Movie
+      </Link>
+    </View>
+  )
+}
+
+export default Home
